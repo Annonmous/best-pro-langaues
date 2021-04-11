014e7d88e312279216acc410bb487badf909700a
Muhammad Huzaifah <mhuzaifahkhan397@gmail.com>
2021-04-12 02:21:51 +0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <>
+  <h1>Best Programming Languages</h1>
+  <p>The List Are Following Below</p>
+  <ol>
+    <li>Java Script</li>
+    <li>Python</li>
+    <li>Swift</li>
+    <li>C#</li>
+  </ol>
+  </>,
+  document.getElementById('root')
+);
+
+
